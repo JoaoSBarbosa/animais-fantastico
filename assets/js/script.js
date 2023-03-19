c11cc8a4d77458bdc59aa715b2cb464fd8b33992
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-18 23:23:53 -0300

    Tratamento botão mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const btnMobile = document.getElementById("btn-mobile");
-function toggleMenu(){
+function toggleMenu(event){
+   if(event.type === 'touchstart') event.preventDefault();
    const nav = document.getElementById("nav");
    nav.classList.toggle('ativo');
+   const active = nav.classList.contains("ativo");
+   event.currentTarget.setAttribute("aria-expanded", active);
 }
+
 btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener("touchstart", toggleMenu);
