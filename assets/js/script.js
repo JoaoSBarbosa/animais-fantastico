99546622644e56e1b4d7e420bedeabe3b8a1a109
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-28 17:42:42 -0300

    Otimização contra erros - Verificação se existe elementos

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,17 +27,21 @@ logo.addEventListener("mouseout", logoOut);
 // Lista de imagens
 const tabMenu = document.querySelectorAll(".js-tabmenu li");
 const tabConteudo = document.querySelectorAll(".js-tabcontent section");
-tabConteudo[0].classList.add('ativo');
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener("click", () => {
-    activeTab(index);
-  });
-});
+if (tabMenu.length && tabConteudo.length) {
 
-function activeTab(index) {
-  tabConteudo.forEach((section) => {
-    section.classList.remove("ativo");
+  tabConteudo[0].classList.add("ativo");
+  
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
   });
-  tabConteudo[index].classList.add("ativo");
+
+  function activeTab(index) {
+    tabConteudo.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabConteudo[index].classList.add("ativo");
+  }
 }
