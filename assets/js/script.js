2a700dd4f04cb92695ef6c67ef1afb1d6f2ccf7d
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-28 17:35:21 -0300

    Efeito Javascript- Animais-lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,43 @@
+// Menu Mobile
 const btnMobile = document.getElementById("btn-mobile");
-function toggleMenu(event){
-   if(event.type === 'touchstart') event.preventDefault();
-   const nav = document.getElementById("nav");
-   nav.classList.toggle('ativo');
-   const active = nav.classList.contains("ativo");
-   event.currentTarget.setAttribute("aria-expanded", active);
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("ativo");
+  const active = nav.classList.contains("ativo");
+  event.currentTarget.setAttribute("aria-expanded", active);
 }
 
-btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+// Hover menu
+const logo = document.querySelector(".logo-menu img");
+
+function logoHover() {
+  logo.src = "assets/img/wolf3.png";
+}
+
+function logoOut() {
+  logo.src = "assets/img/wolf.png";
+}
+
+logo.addEventListener("mouseover", logoHover);
+logo.addEventListener("mouseout", logoOut);
+
+// Lista de imagens
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabConteudo = document.querySelectorAll(".js-tabcontent section");
+tabConteudo[0].classList.add('ativo');
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener("click", () => {
+    activeTab(index);
+  });
+});
+
+function activeTab(index) {
+  tabConteudo.forEach((section) => {
+    section.classList.remove("ativo");
+  });
+  tabConteudo[index].classList.add("ativo");
+}
