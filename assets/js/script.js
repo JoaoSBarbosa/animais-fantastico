c64fdca5e95e5b6aacf0420609e94b17eb158db4
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-30 17:13:07 -0300

    Efeito de scroll- Projeto 100%

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,21 @@ function toggleMenu(event) {
   if (event.type === "touchstart") event.preventDefault();
   const nav = document.getElementById("nav");
   nav.classList.toggle("ativo");
+
+  const links = document.querySelectorAll('.js-menu a[href^="#"');
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("ativo");
+    });
+  });
+
   const active = nav.classList.contains("ativo");
   event.currentTarget.setAttribute("aria-expanded", active);
 }
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
 // Hover menu
 const logo = document.querySelector(".logo-menu img");
 
@@ -87,3 +96,45 @@ function scrollToSection(e) {
 linksInterno.forEach((link) => {
   link.addEventListener("click", scrollToSection);
 });
+
+// Animação de scroll
+function initAnimationScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimationScroll();
+// Scroll
+
+function subirTela() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function displayBtn() {
+  if (window.scrollY === 0) {
+    document.querySelector(".scroll-btn").style.display = "none";
+  } else {
+    document.querySelector(".scroll-btn").style.display = "flex";
+  }
+}
+
+window.addEventListener("scroll", displayBtn);
